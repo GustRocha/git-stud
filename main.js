000428eaf1fabbb1f093d3f1bb67be05120c32b1
Gustavo Rocha <gustavo.santos@dafiti.com.br>
2019-09-06 01:37:51 -0300

    Function

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-var lista = ["Lua Sangrenta", "Death Star", "Project"];
-
-for (i in lista){
-    alert(lista [i]);
+function soma(a, b){
+    alert(a+b);
 }
+
+//soma(5, 5);
+
+function sub(a, b){
+    return a-b;
+}
+var s = sub(10, 4);
+//alert(s);
+
+function div(a,b){
+    return a/b;
+
+}
+alert(div(10,2));
